4afbffe8fc9115b7f8bad10b2a6fc03be5e75ec2
Tomasz Martysk <tomek@martynski.pl>
2019-01-31 22:52:21 +0100

    byteLength fix

--- src/codecs/DateCodec.ts
+++ src/codecs/DateCodec.ts
@@ -3,13 +3,13 @@ import { ICustomFieldCodec } from "./ICustomFieldCodec";
 export class DateCodec implements ICustomFieldCodec<Date> {
   public encode(date: Date) {
     const b = Buffer.alloc(8);
-    b.writeUIntLE(date.getTime(), 0, 8);
+    b.writeUIntLE(date.getTime(), 0, 6);
 
     return b;
   }
 
   public decode(buffer: Buffer): Date {
-    const utcTimestamp = buffer.readUIntLE(0, 8);
+    const utcTimestamp = buffer.readUIntLE(0, 6);
 
     return new Date(utcTimestamp);
   }
